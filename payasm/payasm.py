4d989d247d2f8da83b44ece554ed56fbe4ad66a4
Krishna Ram Prakash R <krp@gtux.in>
2017-05-30 23:02:49 +0530

    Added function parsing and better arg_val parsing

--- payasm/payasm.py
+++ payasm/payasm.py
@@ -5,9 +5,12 @@ from types import CodeType
 import struct
 import marshal
 import ast
+import re
 
 class Instruction:
-    def __init__(self, inst):
+    def __init__(self, inst, function):
+        print inst
+        self.function = function
         self.raw = inst
         inst = inst.split()
         self.opcode = inst[0]
@@ -16,9 +19,19 @@ class Instruction:
         self.has_arg = self.opbyte >= 0x5a
         if self.has_arg:
             self.arg = int(inst[1])
+            arg_val = ' '.join(inst[2:]).strip()
             try:
-                self.arg_val = ast.literal_eval(' '.join(inst[2:]))
-            except KeyError:
+                if arg_val != '':
+                    self.arg_val = ast.literal_eval(arg_val)
+            #Handling names for LOAD_FAST and STORE_FAST
+            #Both local variable identifiers and method names
+            except ValueError:
+                parts = re.findall("\((.*)\)", arg_val)
+                if(len(parts) == 1):
+                    self.arg_val = parts[0]
+            #Handling comment-like args, like in SETUP_LOOP
+            #where terminal instruction number is the arg_val
+            except SyntaxError:
                 pass
 
     def __repr__(self):
@@ -39,15 +52,32 @@ def populate_args(inst):
     else:
         return ''
 
-def assemble(disasm, target_name):
-    code = ''
-    insts = [inst[WHITESPACE_LEN:] for inst in disasm.split('\n')]
-    constants = {}
+def parse_instructions(disasm):
+    insts = disasm.split('\n')
+    fn_name = None
+    instructions = []
     for inst in insts:
         if inst.strip() == '':
             continue
-        inst = Instruction(inst)
 
+        #Check if a new function is starting
+        fn_lt = re.findall("Disassembly of(.*):", inst)
+        if len(fn_lt) == 1:
+            fn_name = fn_lt[0]
+            continue
+
+        #Sanitize the whitespace and line number markers
+        #TODO line number support for better stacktrace and decompilation
+        inst = inst[WHITESPACE_LEN:]
+
+        instructions.append(Instruction(inst, fn_name))
+    return instructions
+
+
+def assemble(instructions, target_name):
+    constants = {}
+    code = ''
+    for inst in instructions:
         #Generating bytecode
         code += chr(inst.opbyte)
         code += populate_args(inst)
@@ -58,7 +88,7 @@ def assemble(disasm, target_name):
             constants[index] = value
             continue
 
-    #populating constants tuple from the dict
+    #populating constants from the dict to list
     const_list = []
     try:
         i = 0
@@ -91,8 +121,7 @@ def assemble(disasm, target_name):
             firstlineno,
             lnotab
     )
-    write_pyc(code_type, target_name + '.pyc')
-
+    return code_type
 
 def write_pyc(code, location):
     py_magic = '\x03\xf3\x0d\x0a'
--- tests/asm.py
+++ tests/asm.py
@@ -1,6 +1,5 @@
 import unittest
 from subprocess import check_output
-import os
 from payasm import payasm
 
 class PayasmTest(unittest.TestCase):
@@ -10,9 +9,17 @@ class PayasmTest(unittest.TestCase):
         filename = "tests/disasm-hello-world"
         with open(filename, 'r') as fp:
             disasm = fp.read()
-            payasm.assemble(disasm, "tests/hello-world")
-    
+        instructions = payasm.parse_instructions(disasm)
+        code = payasm.assemble(instructions, "tests/hello-world")
+        payasm.write_pyc(code, "tests/hello-world.pyc")
+
         output = check_output(['python2', 'tests/hello-world.pyc'])
         self.assertEqual("Hello World!\n", output)
-     
+
+    def test_armstrong(self):
+        filename = "tests/disasm_simple_armstrong"
+        with open(filename, 'r') as fp:
+            disasm = fp.read()
+        instructions = payasm.parse_instructions(disasm)
+        payasm.assemble(instructions, "tests/simple_armstrong")
 
